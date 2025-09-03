685a7d9ad6e958be240f836d229c5308a0de964c
nayraarr <zita.nayra.ardini@gmail.com>
2025-09-03 14:55:56 +0700

    Mengonfigurasikan Routing URL

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406404913',
+        'name': 'Zita Nayra Ardini',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
